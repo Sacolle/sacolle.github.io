7f55cb11296c0debba971f063ecedfa82e4a3c45
Sacolle <73004469+Sacolle@users.noreply.github.com>
2020-10-21 12:57:54 -0300

    Update game.js

--- tictactoe/game.js
+++ tictactoe/game.js
@@ -25,27 +25,35 @@ function changeCharacter(element) {
 
         if(placementX.includes("a")&&placementX.includes("b")&&placementX.includes("c")){
             winning("X");
+            return;
         }
         if(placementX.includes("d")&&placementX.includes("e")&&placementX.includes("f")){
             winning("X");
+            return;
         }
         if(placementX.includes("g")&&placementX.includes("h")&&placementX.includes("i")){
             winning("X");
+            return;
         }
         if(placementX.includes("a")&&placementX.includes("d")&&placementX.includes("g")){
             winning("X");
+            return;
         }
         if(placementX.includes("b")&&placementX.includes("e")&&placementX.includes("h")){
             winning("X");
+            return;
         }
         if(placementX.includes("c")&&placementX.includes("f")&&placementX.includes("i")){
             winning("X");
+            return;
         }
         if(placementX.includes("a")&&placementX.includes("e")&&placementX.includes("i")){
             winning("X");
+            return;
         }
         if(placementX.includes("c")&&placementX.includes("e")&&placementX.includes("g")){
             winning("X");
+            return;
         }
     }else{
         element.textContent = "O";
@@ -54,27 +62,35 @@ function changeCharacter(element) {
 
         if(placementO.includes("a")&&placementO.includes("b")&&placementO.includes("c")){
             winning("O");
+            return;
         }
         if(placementO.includes("d")&&placementO.includes("e")&&placementO.includes("f")){
             winning("O");
+            return;
         }
         if(placementO.includes("g")&&placementO.includes("h")&&placementO.includes("i")){
             winning("O");
+            return;
         }
         if(placementO.includes("a")&&placementO.includes("d")&&placementO.includes("g")){
             winning("O");
+            return;
         }
         if(placementO.includes("b")&&placementO.includes("e")&&placementO.includes("h")){
             winning("O");
+            return;
         }
         if(placementO.includes("c")&&placementO.includes("f")&&placementO.includes("i")){
             winning("O");
+            return;
         }
         if(placementO.includes("a")&&placementO.includes("e")&&placementO.includes("i")){
-            winning("O");        
+            winning("O"); 
+            return;       
         }
         if(placementO.includes("c")&&placementO.includes("e")&&placementO.includes("g")){
             winning("O");
+            return;
         }
     }
     
