20db38332ae5d5d4b6b599cf584fa60d76316052
Sacolle <73004469+Sacolle@users.noreply.github.com>
2021-03-04 22:03:41 -0300

    Update script.js

--- siteParaOsLink/script.js
+++ siteParaOsLink/script.js
@@ -2,7 +2,7 @@ let Aulas = [
     M1 = {nome : "Matemática Discreta", horario : "8:00", dias : ["segunda","quarta"], linkZoom : "https://pucrs.zoom.us/j/94542064876?pwd=UU00Um1ENHN1SXVWU2QwTDhQQlVIUT09"},
     M2 = {nome : "Fundamentos da programação", horario : "9:45", dias : ["segunda", "quarta", "sexta"], linkZoom : "https://pucrs.zoom.us/j/98414194821?pwd=by9LcFp5Z1NHMWxOMk5CTDZGYzRPQT09"},
     M3 = {nome : "Cálculo", horario : "11:30", dias : ["terça", "sexta"], linkZoom : "https://pucrs.zoom.us/j/97173996206?pwd=alJEbTBBV3IwUDVQNFRiM3hEWVkzQT09"},
-    M4 = {nome : "Metodologia Científica", horario : "8:00" , dias : ["terça"], linkZoom : null},
+    M4 = {nome : "Metodologia Científica", horario : "8:00" , dias : ["terça"], linkZoom : "https://pucrs.zoom.us/j/3180016465?pwd=SUdSYnNBa0x0Tm0xN2NsOXp2Ly9wZz09"},
     M5 = {nome : "Introdução a Computação", horario : "9:45", dias : ["terça"], linkZoom : "https://pucrs.zoom.us/j/94547154921?pwd=VEYxVVUvalFjWTZmTzdYeldWMjZNdz09"}
 ];
 
